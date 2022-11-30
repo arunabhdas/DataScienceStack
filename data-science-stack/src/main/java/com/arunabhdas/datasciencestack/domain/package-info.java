cd3c873a494d4cfad2651842884f0ff7f7cc5e29
Arunabh Das <arunabhdas@gmail.com>
2022-11-30 06:51:34 -0500

    Ran ==> jhipster

---
+++ data-science-stack/src/main/java/com/arunabhdas/datasciencestack/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.arunabhdas.datasciencestack.domain;
---
+++ data-science-stack/src/main/java/com/arunabhdas/datasciencestack/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.arunabhdas.datasciencestack.service.dto;
