7b4cb3dcfc48eb354c4ee971f73ac89787902df7
Arunabh Das <arunabhdas@gmail.com>
2022-12-01 06:02:25 -0500

    Cleanup

--- data-science-stack-deprecated/src/main/java/com/arunabhdas/datasciencestack/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arunabhdas.datasciencestack.repository;
-
-import com.arunabhdas.datasciencestack.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
